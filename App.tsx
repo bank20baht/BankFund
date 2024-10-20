db9ba650b6480f5973f2a7416dd18ccb7187577d
Nattapong Promthong <banker.z412@gmail.com>
2024-10-20 21:18:03 +0700

    feat: add react native reanimated to this project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,51 @@
 import { Button, SafeAreaView, Text, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 import Header from "./src/components/Header";
 import useCountStore from "./src/store/store";
+import { styled } from "nativewind";
+
+const AnimatedView = styled(Animated.View);
 
 export default function App() {
   const { count, increaseCount, decreaseCount } = useCountStore();
 
+  const width = useSharedValue(100);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: width.value,
+    };
+  });
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
+  const handlePressDecrees = () => {
+    width.value = withSpring(width.value - 50);
+  };
+
   return (
     <SafeAreaView className="flex-1">
       <Header />
-      <View>
+      <View className="p-4">
         <Text>Count: {count}</Text>
-        <Text className="">Hello</Text>
+        <Text className="text-lg">Hello</Text>
         <Button title="Increase" onPress={increaseCount} />
         <Button title="Decrease" onPress={decreaseCount} />
+      </View>
+
+      <View className="flex items-center justify-center mt-4">
+        <AnimatedView className="bg-violet-500 h-24" style={animatedStyle} />
+      </View>
+
+      <View className="flex-row justify-around mt-4">
+        <Button onPress={handlePress} title="Expand" />
+        <Button onPress={handlePressDecrees} title="shrink" />
       </View>
     </SafeAreaView>
   );
